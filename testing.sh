4768667bd76b5160c4fbd7de817ecab5d0c7f152
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2022-10-11 23:29:55 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
   #!/bin/bash
   pip install -r requirements.txt
   pip install gunicorn
-  nohup python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon 
+  python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon 
