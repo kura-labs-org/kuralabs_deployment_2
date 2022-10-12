3a4ff3215d1e6a6b7dd4ebf598058efbd276284b
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2022-10-12 00:06:24 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
   #!/bin/bash
-  pip install -r requirements.txt
-  pip install gunicorn
-  python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon 
+ 
+ps aux | grep -i "gunicorn" | tr -s " " | head -n 1 | cut  -d " " -f 2 > pid.txt
+kill $(cat pid.txt)
